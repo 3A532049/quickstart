a9e6bcf5aa6fdee74938bffee1cb51f9676e90de
3A532049 <shelly213334@gmail.com>
2018-10-19 15:21:43 +0800

    app.blade.php

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,26 @@
+// resources/views/layouts/app.blade.php
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Laravel 快速入門 - 基本</title>
+
+    <!-- CSS 及 JavaScript -->
+</head>
+
+<body>
+<div class="container">
+    <nav class="navbar navbar-default">
+        <!-- Navbar 內容 -->
+    </nav>
+</div>
+
+@yield('content')
+</body>
+</html>
+/**
+ * Created by PhpStorm.
+ * User: 游智萱
+ * Date: 2018/10/19
+ * Time: 下午 03:20
+ */
